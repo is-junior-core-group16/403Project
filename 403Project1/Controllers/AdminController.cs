cac3594d1608cfd57d56fb423b31de48f6467c6d
Haley <dunlop.haley95@gmail.com>
2018-11-11 18:49:19 -0700

    added action results to admin controller

--- 403Project1/Controllers/AdminController.cs
+++ 403Project1/Controllers/AdminController.cs
@@ -9,7 +9,27 @@ namespace _403Project1.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
+
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        //admin view to simply see the requests, use page that Kyle created
+        public ActionResult ShowAllRequests()
+        {
+            return View(ShowList);
+        }
+
+        //show the black list (hard coded, add to database for later)
+        public ActionResult ShowBlackList()
+        {
+            return View();
+        }
+
+
+        //show the white list (hard coded, add to database for later)
+        public ActionResult ShowWhiteList()
         {
             return View();
         }
