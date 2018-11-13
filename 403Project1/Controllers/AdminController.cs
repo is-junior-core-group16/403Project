d7cca6d5c00291d105247ccdddadfd5e5f905209
Kyle <kylebharrison16@gmail.com>
2018-11-12 21:47:35 -0700

    Merged Haley's part into the project

--- 403Project1/Controllers/AdminController.cs
+++ 403Project1/Controllers/AdminController.cs
@@ -9,37 +9,85 @@ namespace _403Project1.Controllers
 {
     public class AdminController : Controller
     {
-        public static List<Requests> requests = new List<Requests>();
-        public static Requests myreq = new Requests();
-        public static List<String> Whitelist = new List<String>();
-        public static List<String> Blacklist = new List<String>();
 
-        // GET: Admin
-        //[Authorize]
+        //Get:Home
+        public static List<AllRequests> listRequests = new List<AllRequests>();
+        public static AllRequests edit = new AllRequests();
+        public static AllRequests listitem1 = new AllRequests();
+
+
         public ActionResult Index()
+
         {
             return View();
         }
 
-        //admin view to simply see the requests, use page that Kyle created (question for Kyle: how do I use list to show all requests?)
-        //[Authorize]
-        public ActionResult ShowList()
+        public ActionResult LandingPage()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(AllRequests myRequest)
+        {
+
+            myRequest.RequestID = (myRequest.Name.Substring(0, 2) + listRequests.Count().ToString());
+            listRequests.Add(myRequest);
+            return View("Thanks");
+        }
+
+        public ActionResult DisplayRequests()
+        {
+            if (!listRequests.Exists(item => item.RequestID == "5A"))
+            {
+                listitem1.Name = "Kyle Harrison";
+                listitem1.RequestID = "5A";
+                listitem1.SoftwareRequest = "Kitty++";
+                listitem1.SoftwareType = "Operating System";
+                listitem1.ListType = "Black List";
+                listRequests.Add(listitem1);
+            }
+
+            return View(listRequests);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            listRequests.RemoveAll(item => item.RequestID == id);
+            return View("DisplayRequests", listRequests);
+        }
+
+
+        public ActionResult Edit(string id)
+        {
+            edit = listRequests.Find(item => item.RequestID == id);
+            return View("Edit", edit);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(AllRequests update)
+        {
+            listRequests.RemoveAll(item => item.RequestID == update.RequestID);
+            listRequests.Add(update);
+            return View("DisplayRequests", listRequests);
+        }
+
 
         //show the black list (hard coded, add to database for later)
-        //[Authorize]
+
         public ActionResult ShowBlackList()
         {
             return View();
         }
 
-        //[Authorize]
+
         //show the white list (hard coded, add to database for later)
         public ActionResult ShowWhiteList()
         {
             return View();
         }
+
+
+
     }
 }
---
+++ 403Project1/Models/AllRequests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _403Project1.Models
+{
+    public class AllRequests
+    {
+        [DisplayName("Request ID")]
+        [Key]
+        public string RequestID { get; set; }
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [DisplayName("Software Request")]
+        public string SoftwareRequest { get; set; }
+        [DisplayName("Software Type")]
+        public string SoftwareType{ get; set; }
+        [DisplayName("List Type")]
+        public string ListType { get; set; }
+    }
+}
